1fbf28fa554a09f95af3add0813393043a974045
srabonx <srabon9012@gmail.com>
2024-07-17 02:07:45 +0600

    server page navigation

---
+++ ServerManagement/Components/Models/CityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerManagement.Components.Models;
+
+public static class CityRepository
+{
+    public static List<string> Cities = new List<string>()
+    {
+        "Toronto",
+        "Montreal",
+        "Halifax",
+        "Calgary",
+        "Ottawa"
+    };
+
+    public static List<string> GetCities()
+    {
+        return Cities;
+    }
+}
